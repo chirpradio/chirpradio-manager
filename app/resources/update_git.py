8a901736e857cb1a602469d87fc2613a29c918fd
mcfstr <matt.foster.c@gmail.com>
2013-06-08 16:11:16 -0700

    fix git flags

--- app/resources/update_git.py
+++ app/resources/update_git.py
@@ -5,7 +5,7 @@ from flask import current_app as app
 from chirp.library import artists
 
 def update():
-    GIT_DIR = app.config['GIT_DIR']
-    WORK_TREE = app.config['WORK_TREE']
+    GIT_DIR = '--git-dir=' + app.config['GIT_DIR']
+    WORK_TREE = '--work-tree=' + app.config['WORK_TREE']
     sh.git(GIT_DIR, WORK_TREE, 'commit', artists._WHITELIST_FILE, '-m', 'Adding new artists')
     sh.git(GIT_DIR, WORK_TREE, 'push')
