61bdd3b0898b7ed672a92c620e77e2e8664a0f7c
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-06-20 15:43:04 -0500

    catch exception for reading albums in inbox

--- app/resources/importalbums.py
+++ app/resources/importalbums.py
@@ -28,7 +28,7 @@ VOLUME_NUMBER = 1
 IMPORT_SIZE_LIMIT = 0.95 * (3 << 30) # 95% of 3GB.
 
 class ImportAlbums(Resource):
-     
+
     def import_albums(self, inbox):
         # do_periodic_import.py
         prescan_timestamp = timestamp.now()
@@ -37,88 +37,92 @@ class ImportAlbums(Resource):
         seen_fp = {}
         albums = []
         db = database.Database(LIBRARY_DB)
-        
+
         try:
             for i, alb in enumerate(inbox.albums()):
-                albm = {'album_id': i+1, 'tracks': [], 'messages': []}
-                alb.drop_payloads()
-                albm['title'] = alb.title()
-                albm['artist'] = alb.all_artist_names() if alb.is_compilation() else alb.artist_name() 
-                album_count += 1
-
-                logging.info("#%d" % album_count)
-                logging.info((u'"%s"' % alb.title()).encode('utf-8'))
-                if alb.tags():
-                    logging.info("(%s)" % ", ".join(alb.tags()))
-                else:
-                    logging.info('\n')
-                duration_ms = sum(au.duration_ms for au in alb.all_au_files)
-                if alb.is_compilation():
-                    logging.info("Compilation")
-                    for i, au in enumerate(alb.all_au_files):
-                        logging.info(" %02d:" % (i+1,)+unicode(au.mutagen_id3["TPE1"]).encode("utf-8"))
-                else:
-                    logging.info(alb.artist_name().encode("utf-8"))
-                logging.info("%d tracks / %d minutes" % (len(alb.all_au_files), int(duration_ms / 60000)))
-                logging.info("ID=%015x" % alb.album_id)
-                 
-                # Check that the album isn't already in library.
-                collision = False
-                for au in alb.all_au_files:
-                    # create track
-                    track = {}
-                    track['path'] = au.path
-                    try:
-                        track['number'] = au.mutagen_id3['TRCK'].text[0].split('/')[0]
-                        track['title'] = au.tit2()
-                        track['artist'] = au.tpe1()
-                        track['album_id'] = albm['album_id']
-                    except Exception, e:
-                        logging.exception(e)
-                        albm['messages'].append({'message': "error reading dropbox", 'status': 'error'})
-                        albm['status'] = "error"
-                        error_count += 1
-                    album_path = au.path
-                    albm['path'] = '/'.join(album_path.split('/')[:-1])
-                    
-                    if au.fingerprint in seen_fp:
-                        message = ("***** ERROR: DUPLICATE TRACK WITHIN IMPORT\n"
-                                  ("This one is at %s\n" % au.path)
-                                  ("Other one is at %s" % seen_fp[au.fingerprint].path))
-                        logging.error(message)
-                        collision = True
-                        # break
-                    fp_au_file = db.get_by_fingerprint(au.fingerprint)
-                    if fp_au_file is not None:
-                        message = ("****** ERROR: TRACK ALREADY IN LIBRARY\n"+
-                                   unicode(fp_au_file.mutagen_id3).encode("utf-8"))
-                        logging.error(message)
-                        collision = True
-                        # break
-                    seen_fp[au.fingerprint] = au
-                    
-                    albm['tracks'].append(track)
-                    
-                if collision:
-                    error_count += 1
-                    albm['messages'].append({'message': 'Tracks already in library', 'status': 'error'})
-                    albm['status'] = 'error'
-
-                # Attach a dummy volume # and timestamp
-                alb.set_volume_and_import_timestamp(0xff, prescan_timestamp)
                 try:
-                    alb.standardize()
-                    logging.info("OK!")
-                except (import_file.ImportFileError, album.AlbumError), ex:
-                    message = ("***** IMPORT ERROR"
-                               "***** %s\n" % str(ex))
-                    logging.error(message)
-                    albm['messages'].append({'message': message, 'status': 'error'})
-                    albm['status'] = 'error'
-                    error_count += 1
-           
-                albums.append(albm)
-            
+                    #TODO handle excections so that error albums don't silently disapper
+                    albm = {'album_id': i+1, 'tracks': [], 'messages': []}
+                    alb.drop_payloads()
+                    albm['title'] = alb.title()
+                    albm['artist'] = alb.all_artist_names() if alb.is_compilation() else alb.artist_name()
+                    album_count += 1
+
+                    logging.info("#%d" % album_count)
+                    logging.info((u'"%s"' % alb.title()).encode('utf-8'))
+                    if alb.tags():
+                        logging.info("(%s)" % ", ".join(alb.tags()))
+                    else:
+                        logging.info('\n')
+                    duration_ms = sum(au.duration_ms for au in alb.all_au_files)
+                    if alb.is_compilation():
+                        logging.info("Compilation")
+                        for i, au in enumerate(alb.all_au_files):
+                            logging.info(" %02d:" % (i+1,)+unicode(au.mutagen_id3["TPE1"]).encode("utf-8"))
+                    else:
+                        logging.info(alb.artist_name().encode("utf-8"))
+                    logging.info("%d tracks / %d minutes" % (len(alb.all_au_files), int(duration_ms / 60000)))
+                    logging.info("ID=%015x" % alb.album_id)
+
+                    # Check that the album isn't already in library.
+                    collision = False
+                    for au in alb.all_au_files:
+                        # create track
+                        track = {}
+                        track['path'] = au.path
+                        try:
+                            track['number'] = au.mutagen_id3['TRCK'].text[0].split('/')[0]
+                            track['title'] = au.tit2()
+                            track['artist'] = au.tpe1()
+                            track['album_id'] = albm['album_id']
+                        except Exception, e:
+                            logging.exception(e)
+                            albm['messages'].append({'message': "error reading dropbox", 'status': 'error'})
+                            albm['status'] = "error"
+                            error_count += 1
+                        album_path = au.path
+                        albm['path'] = '/'.join(album_path.split('/')[:-1])
+
+                        if au.fingerprint in seen_fp:
+                            message = ("***** ERROR: DUPLICATE TRACK WITHIN IMPORT\n"
+                                      ("This one is at %s\n" % au.path)
+                                      ("Other one is at %s" % seen_fp[au.fingerprint].path))
+                            logging.error(message)
+                            collision = True
+                            # break
+                        fp_au_file = db.get_by_fingerprint(au.fingerprint)
+                        if fp_au_file is not None:
+                            message = ("****** ERROR: TRACK ALREADY IN LIBRARY\n"+
+                                       unicode(fp_au_file.mutagen_id3).encode("utf-8"))
+                            logging.error(message)
+                            collision = True
+                            # break
+                        seen_fp[au.fingerprint] = au
+
+                        albm['tracks'].append(track)
+
+                    if collision:
+                        error_count += 1
+                        albm['messages'].append({'message': 'Tracks already in library', 'status': 'error'})
+                        albm['status'] = 'error'
+
+                    # Attach a dummy volume # and timestamp
+                    alb.set_volume_and_import_timestamp(0xff, prescan_timestamp)
+                    try:
+                        alb.standardize()
+                        logging.info("OK!")
+                    except (import_file.ImportFileError, album.AlbumError), ex:
+                        message = ("***** IMPORT ERROR"
+                                   "***** %s\n" % str(ex))
+                        logging.error(message)
+                        albm['messages'].append({'message': message, 'status': 'error'})
+                        albm['status'] = 'error'
+                        error_count += 1
+
+                    albums.append(albm)
+                except Exception, e:
+                    logging.exception(e)
+
         except analyzer.InvalidFileError, ex:
             message = ("***** INVALID FILE ERROR"
                        "***** %s\n" % str(ex))
@@ -127,7 +131,7 @@ class ImportAlbums(Resource):
 
         logging.info("-" * 40)
         logging.info("Found %d albums" % album_count)
-        
+
         # sort albums by title, then artist
         albums = sorted(albums, key=lambda album: album.get('title'))
         albums = sorted(albums, key=lambda album: album.get('artist'))
@@ -141,6 +145,7 @@ class ImportAlbums(Resource):
             logging.error("Saw %d errors" % error_count)
             return {'albums': albums}
         else:
+            raw_intput()
             txn = None
             for alb in inbox.albums():
                 if txn is None:
@@ -159,13 +164,14 @@ class ImportAlbums(Resource):
 
             # empty_dropbox.py
             for dir in os.listdir(conf.MUSIC_DROPBOX):
+
                 if dir.startswith('.'):
                     continue
                 fn = os.path.join(conf.MUSIC_DROPBOX, dir)
                 if not os.path.isdir(fn):
                     continue
                 shutil.rmtree(fn)
-            
+
             return {'albums': albums}
 
     def get(self):
@@ -173,9 +179,12 @@ class ImportAlbums(Resource):
         drop = dropbox.Dropbox()
         new_artists = set()
         for au_file in drop.tracks():
-            tpe1 = au_file.mutagen_id3["TPE1"].text[0]
-            if artists.standardize(tpe1) is None:
-                new_artists.add(tpe1)
+            try:
+                tpe1 = au_file.mutagen_id3["TPE1"].text[0]
+                if artists.standardize(tpe1) is None:
+                    new_artists.add(tpe1)
+            except Exception, e:
+                logging.exception(e)
         to_print = list(new_artists)
         to_print.extend(artists.all())
         to_print.sort(key=artists.sort_key)
