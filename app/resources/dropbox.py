d57fe6685e37ef920fa8da7661c4d60c6fd16449
mcfstr <matt.foster.c@gmail.com>
2013-06-29 12:11:49 -0700

    catch exceptions when scanning the dropbox tracks

--- app/resources/dropbox.py
+++ app/resources/dropbox.py
@@ -19,18 +19,23 @@ class Dropbox(Resource):
             result = sorted(albums, key=lambda album: album.get('title'))
             result = sorted(result, key=lambda album: album.get('artist'))
             return result
-
-        for au_file in drop.tracks():
-            try:
-                tpe1 = au_file.mutagen_id3['TPE1'].text[0] # artist
-                tit2 = au_file.mutagen_id3['TIT2'].text[0] # track title
-                trck = au_file.mutagen_id3['TRCK'].text[0].split('/')[0] # track number
-                talb = au_file.mutagen_id3['TALB'].text[0] # album
-                track = {'number': trck, 'title': tit2, 'artist': tpe1, 'album': talb, 'path': au_file.path}
-                tracks.append(track)
-            except Exception, e:
-                tracks.append({'path': au_file.path})
-                logging.exception(e)
+        try:
+            for au_file in drop.tracks():
+                try:
+                    tpe1 = au_file.mutagen_id3['TPE1'].text[0] # artist
+                    tit2 = au_file.mutagen_id3['TIT2'].text[0] # track title
+                    trck = au_file.mutagen_id3['TRCK'].text[0].split('/')[0] # track number
+                    talb = au_file.mutagen_id3['TALB'].text[0] # album
+                    track = {'number': trck, 'title': tit2, 'artist': tpe1, 'album': talb, 'path': au_file.path}
+                    tracks.append(track)
+                except Exception, e:
+                    tracks.append({'path': au_file.path, 'artist': '*error*', 'album': au_file.path})
+                    logging.exception(e)
+        except Exception, e:
+            logging.exception(e)
+            message = str(e) + ' ** Please manually remove this album from the dropbox'
+            messages = [{'message': message, 'status': 'error'}]
+            return {'albums': [{'artist': '*error*', 'status': 'error', 'messages': messages, 'tracks': []}]}
         albums = []
         album_titles = set(track.get('album') for track in tracks)
         for i, album_title in enumerate(album_titles):
