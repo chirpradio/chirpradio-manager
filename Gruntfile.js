35af563cf72eade9af4ae4876b355a55d5365693
mcfstr <matt.foster.c@gmail.com>
2013-10-15 12:09:12 -0700

    recompile static assets to work without Flask's template engine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,20 +2,21 @@ module.exports = function(grunt) {
   
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    handlebars: {
+    emberTemplates: {
       compile: {
         options: {
-          namespace: "App.Templates",
+          amd: false,
+          templateBasePath: /app\/static\/hbs\//
         },
         files: {
-          "app/static/js/result.js": "app/static/hbs/*.handlebars"
+          "app/static/js/result.js": "app/static/hbs/*.handlebars",
         }
       }
     }
-  });
+});
 
-  grunt.loadNpmTasks('grunt-contrib-handlebars');
+  grunt.loadNpmTasks('grunt-ember-templates');
   
-  grunt.registerTask('default', ['handlebars']);
+  grunt.registerTask('default', ['emberTemplates']);
 
 };
