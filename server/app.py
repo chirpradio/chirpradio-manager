9aa32ab7db8e52d5c5faffa2010d7845da8305eb
mcfstr <matt.foster.c@gmail.com>
2013-10-30 14:53:10 -0700

    server app updates

--- server/app.py
+++ server/app.py
@@ -1,36 +1,38 @@
-from flask import Flask, send_file
-from flask.ext.restful import Api
+import flask
+from flask.ext import restful
 
-from resources.dropbox import Dropbox
-from resources.generate import GenerateTraktorLibrary
-#from resources.importalbums import ImportAlbums
-from resources.move import MoveAlbum
-#from resources.push import PushToCloud
-from resources.update import UpdateAlbums
-from resources.whitelist import Whitelist
+from resources.do_periodic_import import ImportAlbums
+from resources.messages import GetMessages
+from resources.pre_import_dropbox_scan import ScanDropbox
+from resources.remove import RemoveAlbum
+from resources.whitelist import GetWhitelist
+from resources.current_route import GetCurrentRoute
+
+# TODO IN_PROCESS
+
+# keep the client in sync
+ERROR = False
+
+# create app and api
+app = flask.Flask(__name__)
+api = restful.Api(app)
+
+# register resources
+api.add_resource(GetMessages, '/messages')       
+api.add_resource(GetWhitelist, '/whitelist')       
+api.add_resource(ImportAlbums, '/import_albums')       
+api.add_resource(RemoveAlbum, '/remove_album')       
+api.add_resource(ScanDropbox, '/scan_dropbox')       
+api.add_resource(GetCurrentRoute, '/current_route')       
 
 
-SERVER_NAME = '0.0.0.0'
-SERVER_PORT = 5000
-
-
-app = Flask(__name__)
-app.config.from_object('settings')
-api = Api(app)
-api.add_resource(Dropbox, '/dropbox')       
-api.add_resource(GenerateTraktorLibrary, '/generate')
-#api.add_resource(ImportAlbums, '/import')
-api.add_resource(MoveAlbum, '/move')
-#api.add_resource(PushToCloud, '/push')
-api.add_resource(UpdateAlbums, '/update')
-api.add_resource(Whitelist, '/whitelist')
-
-
+# serve client app at root
 @app.route('/')
 def index():
-    return send_file('index.html')
+    return flask.send_file('index.html')
 
 
 if __name__ == '__main__':
-
-    app.run(SERVER_NAME, SERVER_PORT, debug=True)
+  
+    # serve application
+    app.run('0.0.0.0', 5000, debug=True, threaded=True)
