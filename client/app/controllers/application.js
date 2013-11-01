1ea5e3513146155d0d8fcbc322b9e7d41735e7f7
mcfstr <matt.foster.c@gmail.com>
2013-10-31 17:52:35 -0700

    change polling strategy

--- client/app/controllers/application.js
+++ client/app/controllers/application.js
@@ -1,8 +1,12 @@
 App.ApplicationController = Em.Controller.extend({
-  needs: ['landing', 'dropbox', 'import', 'generate', 'push'],
+  needs: ['landing', 'dropbox', 'import', 'generate', 'push', 'messages'],
   showWhitelist: null,
   actions: { 
     next: function() {
+
+      // clear messages from screen
+      this.get('controllers.messages').clear();
+
       // transition to the next route
       this.transitionToRoute(this.get('controllers.'+this.get('currentRouteName')).nextPath);
     },
