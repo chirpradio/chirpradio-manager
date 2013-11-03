3999ae315287ebed644b2d3277ac813338919f86
mcfstr <matt.foster.c@gmail.com>
2013-11-02 19:07:24 -0500

    hard-coding for CHIRP Studio

--- server/resources/remove.py
+++ server/resources/remove.py
@@ -13,10 +13,9 @@ class RemoveAlbum(Resource):
 
     def post(self):
         album_path = parser.parse_args()['path']
+        remove_from_dropbox = "/home/musiclib/.virtualenvs/chirpradio-machine/bin/remove_from_dropbox"
+        exit_status = subprocess.call("sudo %s %s" % (remove_from_dropbox, album_path), shell=True)
 
-        # TODO hardcode, will not work with virtualenv
-        exit_status = subprocess.call("sudo `which remove_from_dropbox` %s" % album_path, shell=True)
-        
         if exit_status == 0:
             Messages.add_message('Successfully removed album %s from dropbox.' % album_path, 'success')
             return {'success': True}
